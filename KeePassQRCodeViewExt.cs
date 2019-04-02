47acd895a2405f1fb7b55d2a376abadc98ae182d
Pawel Cioch <pawel@greenhex.net>
2019-04-02 10:06:18 -0500

    Visual Studio auto formatting

--- KeePassQRCodeViewExt.cs
+++ KeePassQRCodeViewExt.cs
@@ -15,169 +15,169 @@ using QRCoder;
 
 namespace KeePassQRCodeView
 {
-	public class KeePassQRCodeViewExt : Plugin
-	{
-		private const string CONTEXT_MENU_ITEM_LABEL = "QR Code";
-		private const string INSERT_AFTER_ENTRY_KEY = "m_ctxEntrySaveAttachedFiles";
+    public class KeePassQRCodeViewExt : Plugin
+    {
+        private const string CONTEXT_MENU_ITEM_LABEL = "QR Code";
+        private const string INSERT_AFTER_ENTRY_KEY = "m_ctxEntrySaveAttachedFiles";
 
-		private IPluginHost host;
+        private IPluginHost host;
 
-		private ToolStripMenuItem ctxEntryShowQRCode;
-		private DynamicMenu dynQRCodes;
+        private ToolStripMenuItem ctxEntryShowQRCode;
+        private DynamicMenu dynQRCodes;
 
-		public override Image SmallIcon { get { return Properties.Resources.icon; } }
+        public override Image SmallIcon { get { return Properties.Resources.icon; } }
 
-		public override string UpdateUrl { get { return "https://github.com/JanisEst/KeePassQRCodeView/raw/master/keepass.version"; } }
+        public override string UpdateUrl { get { return "https://github.com/JanisEst/KeePassQRCodeView/raw/master/keepass.version"; } }
 
-		public override bool Initialize(IPluginHost host)
-		{
-			Contract.Requires(host != null);
+        public override bool Initialize(IPluginHost host)
+        {
+            Contract.Requires(host != null);
 
-			this.host = host;
+            this.host = host;
 
-			ctxEntryShowQRCode = new ToolStripMenuItem
-			{
-				Image = Properties.Resources.icon,
-				Text = CONTEXT_MENU_ITEM_LABEL
-			};
-			dynQRCodes = new DynamicMenu(ctxEntryShowQRCode.DropDownItems);
-			dynQRCodes.MenuClick += OnShowQRCode;
+            ctxEntryShowQRCode = new ToolStripMenuItem
+            {
+                Image = Properties.Resources.icon,
+                Text = CONTEXT_MENU_ITEM_LABEL
+            };
+            dynQRCodes = new DynamicMenu(ctxEntryShowQRCode.DropDownItems);
+            dynQRCodes.MenuClick += OnShowQRCode;
 
-			var insertAfterIndex = host.MainWindow.EntryContextMenu.Items.IndexOfKey(INSERT_AFTER_ENTRY_KEY);
-			if (insertAfterIndex != -1)
-			{
-				//insert after "Save Attachements"
-				host.MainWindow.EntryContextMenu.Items.Insert(insertAfterIndex + 1, ctxEntryShowQRCode);
-			}
-			else
-			{
-				//add at the end
-				host.MainWindow.EntryContextMenu.Items.Add(ctxEntryShowQRCode);
-			}
-			host.MainWindow.EntryContextMenu.Opening += OnEntryContextMenuOpening;
+            var insertAfterIndex = host.MainWindow.EntryContextMenu.Items.IndexOfKey(INSERT_AFTER_ENTRY_KEY);
+            if (insertAfterIndex != -1)
+            {
+                //insert after "Save Attachements"
+                host.MainWindow.EntryContextMenu.Items.Insert(insertAfterIndex + 1, ctxEntryShowQRCode);
+            }
+            else
+            {
+                //add at the end
+                host.MainWindow.EntryContextMenu.Items.Add(ctxEntryShowQRCode);
+            }
+            host.MainWindow.EntryContextMenu.Opening += OnEntryContextMenuOpening;
 
-			return true;
-		}
+            return true;
+        }
 
-		public override void Terminate()
-		{
-			host.MainWindow.EntryContextMenu.Opening -= OnEntryContextMenuOpening;
-			host.MainWindow.EntryContextMenu.Items.Remove(ctxEntryShowQRCode);
+        public override void Terminate()
+        {
+            host.MainWindow.EntryContextMenu.Opening -= OnEntryContextMenuOpening;
+            host.MainWindow.EntryContextMenu.Items.Remove(ctxEntryShowQRCode);
 
-			dynQRCodes.MenuClick -= OnShowQRCode;
-		}
+            dynQRCodes.MenuClick -= OnShowQRCode;
+        }
 
-		private void OnEntryContextMenuOpening(object sender, CancelEventArgs e)
-		{
-			ctxEntryShowQRCode.Visible = false;
+        private void OnEntryContextMenuOpening(object sender, CancelEventArgs e)
+        {
+            ctxEntryShowQRCode.Visible = false;
 
-			if (!host.Database.IsOpen)
-			{
-				return;
-			}
+            if (!host.Database.IsOpen)
+            {
+                return;
+            }
 
-			if (host.MainWindow.GetSelectedEntriesCount() != 1)
-			{
-				return;
-			}
+            if (host.MainWindow.GetSelectedEntriesCount() != 1)
+            {
+                return;
+            }
 
-			var pe = host.MainWindow.GetSelectedEntry(true);
-			if (pe == null)
-			{
-				return;
-			}
+            var pe = host.MainWindow.GetSelectedEntry(true);
+            if (pe == null)
+            {
+                return;
+            }
 
-			dynQRCodes.Clear();
+            dynQRCodes.Clear();
 
-			var items = new List<Tuple<string, string>>();
-			foreach (var kvp in pe.Strings)
-			{
-				if (kvp.Value.IsEmpty)
-				{
-					continue;
-				}
+            var items = new List<Tuple<string, string>>();
+            foreach (var kvp in pe.Strings)
+            {
+                if (kvp.Value.IsEmpty)
+                {
+                    continue;
+                }
 
-				items.Add(Tuple.Create(
-					StrUtil.EncodeMenuText(TryTranslateKey(kvp.Key)),
-					kvp.Key
-				));
-			}
+                items.Add(Tuple.Create(
+                    StrUtil.EncodeMenuText(TryTranslateKey(kvp.Key)),
+                    kvp.Key
+                ));
+            }
 
-			foreach (var kv in items.OrderBy(t => t.Item1))
-			{
-				dynQRCodes.AddItem(
-					kv.Item1,
-					Properties.Resources.icon,
-					kv.Item2
-				);
-			}
+            foreach (var kv in items.OrderBy(t => t.Item1))
+            {
+                dynQRCodes.AddItem(
+                    kv.Item1,
+                    Properties.Resources.icon,
+                    kv.Item2
+                );
+            }
 
-			ctxEntryShowQRCode.Visible = true;
-		}
+            ctxEntryShowQRCode.Visible = true;
+        }
 
-		public void OnShowQRCode(object sender, DynamicMenuEventArgs e)
-		{
-			var key = e.Tag as string;
-			if (key == null)
-			{
-				return;
-			}
+        public void OnShowQRCode(object sender, DynamicMenuEventArgs e)
+        {
+            var key = e.Tag as string;
+            if (key == null)
+            {
+                return;
+            }
 
-			var pe = host.MainWindow.GetSelectedEntry(true);
-			if (pe == null)
-			{
-				return;
-			}
+            var pe = host.MainWindow.GetSelectedEntry(true);
+            if (pe == null)
+            {
+                return;
+            }
 
-			var context = new SprContext(pe, host.Database, SprCompileFlags.All);
-			var value = SprEngine.Compile(pe.Strings.GetSafe(key).ReadString(), context);
+            var context = new SprContext(pe, host.Database, SprCompileFlags.All);
+            var value = SprEngine.Compile(pe.Strings.GetSafe(key).ReadString(), context);
 
-			if (key == "otp" && value.StartsWith("key="))
-			{
-				var title = SprEngine.Compile(pe.Strings.GetSafe("Title").ReadString(), context);
-				title = Uri.EscapeDataString(title); 
-				value = string.Format("otpauth://totp/{0}?secret={1}",title,value.Replace("key=", ""));
-			}
-			
-			try
-			{
-				var data = new QRCodeGenerator().CreateQrCode(value, QRCodeGenerator.ECCLevel.L);
-				if (data != null)
-				{
-					var form = new ShowQRCodeForm(
-						host,
-						data.GetBitmap(10, Color.Black, Color.White),
-						SprEngine.Compile(pe.Strings.GetSafe(PwDefs.TitleField).ReadString(), context),
-						TryTranslateKey(key)
-					);
-					form.ShowDialog();
-				}
-			}
-			catch
-			{
-				MessageBox.Show("The data can't be displayed as a QR Code.");
-			}
-		}
+            if (key == "otp" && value.StartsWith("key="))
+            {
+                var title = SprEngine.Compile(pe.Strings.GetSafe("Title").ReadString(), context);
+                title = Uri.EscapeDataString(title);
+                value = string.Format("otpauth://totp/{0}?secret={1}", title, value.Replace("key=", ""));
+            }
 
-		private string TryTranslateKey(string key)
-		{
-			Contract.Requires(key != null);
+            try
+            {
+                var data = new QRCodeGenerator().CreateQrCode(value, QRCodeGenerator.ECCLevel.L);
+                if (data != null)
+                {
+                    var form = new ShowQRCodeForm(
+                        host,
+                        data.GetBitmap(10, Color.Black, Color.White),
+                        SprEngine.Compile(pe.Strings.GetSafe(PwDefs.TitleField).ReadString(), context),
+                        TryTranslateKey(key)
+                    );
+                    form.ShowDialog();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("The data can't be displayed as a QR Code.");
+            }
+        }
 
-			switch (key)
-			{
-				case PwDefs.TitleField:
-					return KPRes.Title;
-				case PwDefs.UserNameField:
-					return KPRes.UserName;
-				case PwDefs.PasswordField:
-					return KPRes.Password;
-				case PwDefs.UrlField:
-					return KPRes.Url;
-				case PwDefs.NotesField:
-					return KPRes.Notes;
-				default:
-					return key;
-			}
-		}
-	}
+        private string TryTranslateKey(string key)
+        {
+            Contract.Requires(key != null);
+
+            switch (key)
+            {
+                case PwDefs.TitleField:
+                    return KPRes.Title;
+                case PwDefs.UserNameField:
+                    return KPRes.UserName;
+                case PwDefs.PasswordField:
+                    return KPRes.Password;
+                case PwDefs.UrlField:
+                    return KPRes.Url;
+                case PwDefs.NotesField:
+                    return KPRes.Notes;
+                default:
+                    return key;
+            }
+        }
+    }
 }
