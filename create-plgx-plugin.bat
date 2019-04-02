f903f9a8e1b6eeb231ff0549567c577cbdc0ec06
Pawel Cioch <pawel@greenhex.net>
2019-04-02 11:27:42 -0500

    Fix windows bash cp into windows copy

--- create-plgx-plugin.bat
+++ create-plgx-plugin.bat
@@ -3,18 +3,19 @@
 set plugin=%cd%\plugin
 set source=%plugin%\source
 					 
-echo coping source files to plugin\source for clean plugin
+echo Copying source files to plugin\source for clean plugin compilation:
 if not exist %source% mkdir %source%
-cp *.cs %source%
-cp *.csproj %source%
-cp *.resx %source% 
-xcopy /S /Y %cd%\Resources %source%\Resources\  >NUL
-xcopy /S /Y %cd%\Properties %source%\Properties\ >NUL
-del %plugin%\*.plgx
+copy /Y *.cs %source%
+copy /Y *.csproj %source%
+copy /Y *.resx %source% 
+xcopy /S /Y %cd%\Resources %source%\Resources\  
+xcopy /S /Y %cd%\Properties %source%\Properties\ 
+del %plugin%\*.plgx >NUL
 	  
-echo compiling plugin
+echo Compiling plugin...
 "c:\Program Files (x86)\KeePass Password Safe 2\KeePass.exe" --plgx-create %source% -plgx-prereq-kp:2.41 --plgx-prereq-net:4.0
 		
-echo renaming
-ren %source%.plgx KeePassQRCodeView.plgx		
+echo Renaming...
+ren %source%.plgx KeePassQRCodeView.plgx	
+echo.
 echo Clean compiled plugin is located in %cd%\plugin
